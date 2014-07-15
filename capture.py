e1c72ee33c31e447b16acdfb1b8a9cd0c0d6993c
Michael Owings <mikey@swampgas.com>
2014-07-15 09:34:42 -0500

    first commit

---
+++ capture.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import time
+import picamera
+import cStringIO
+import os
+import atexit
+from flask import Flask, send_file
+
+app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS'] = True
+camera = picamera.PiCamera()
+camera.rotation=270
+camera.resolution=(2000,2000)
+
+@app.route("/camera/frame.jpg")
+def get_frame():
+    image = capture_frame()
+    image.seek(0)
+    return send_file(image, mimetype='image/jpeg')
+
+def capture_frame():
+    print "Capture starting..."
+    image =  cStringIO.StringIO()
+    camera.capture(image, format='jpeg', quality=100)
+    image.seek(0, os.SEEK_END)
+    print "Capture complete. image is %lu bytes" % image.tell()
+    return image
+
+    camera.close()
+def cleanup():
+    print "cleaned up"
+
+atexit.register(cleanup)
+
+if __name__ == '__main__':
+    app.run()
+
