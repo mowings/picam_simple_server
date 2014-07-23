1db7f60f07781d876268a293b60ae912548277b9
Michael Owings <mikey@swampgas.com>
2014-07-23 11:17:33 -0500

    Add configurable image server

---
+++ camserv.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+import time
+import picamera
+import cStringIO
+import os
+import atexit
+from flask import Flask, send_file, request, redirect, url_for
+
+app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS'] = True
+camera = picamera.PiCamera()
+camera.rotation=270
+camera.resolution=(2592,1944)
+camera.exposure_mode = 'night'
+camera.brightness = 55
+camera_format = 'jpeg'
+camera_quality = 100
+camera_use_video_port = False
+
+@app.route('/camera/settings')
+def settings():
+    for key in request.args.keys():
+        print key
+        if key == 'format' :
+            camera_format = request.args[key]
+        elif key == 'quality':
+            camera_quality = safe_int(request.args[key])
+        elif key == 'use_video_port':
+            camera_use_video_port = safe_bool(request.args[key])
+    return redirect('/camera/frame.jpg')
+
+@app.route("/camera/frame.jpg")
+def get_frame():
+    image = capture_frame()
+    image.seek(0)
+    return send_file(image, mimetype='image/jpeg')
+
+def capture_frame():
+    start = time.time()
+    image =  cStringIO.StringIO()
+    camera.capture(image, use_video_port = camera_use_video_port, format = camera_format, quality = camera_quality)
+    elapsed = time.time() - start
+    image.seek(0, os.SEEK_END)
+    print "Capture complete. image is %lu bytes (%0.3f s)" % (image.tell(), elapsed)
+    return image
+
+def safe_int(val, default):
+    try:
+        return int(val)
+    except ValueError:
+        return default
+
+def safe_bool(val):
+    return val == "True" or val == 1
+
+def rotation(val):
+    pass
+
+def quality(val):
+    pass
+
+def format(val):
+    pass
+
+def use_video_port(val):
+    pass
+
+def exposure_mode(val):
+    pass
+
+def awb_mode(val):
+    pass
+
+def brightness(val):
+    pass
+
+def color_effects(val):
+    pass
+
+def contrast(val):
+    pass
+
+def crop(val):
+    pass
+
+def drc_strength(val):
+    pass
+
+def exposure_compensation(val):
+    pass
+
+def image_effect(val):
+    pass
+
+def led(val):
+    pass
+
+def meter_mode(val):
+    pass
+
+def resolution(val):
+    pass
+
+def saturation(val):
+    pass
+
+def sharpness(val):
+    pass
+
+def shutter_speed(val):
+    pass
+
+def cleanup():
+    camera.close()
+    print "cleaned up"
+
+atexit.register(cleanup)
+
+if __name__ == '__main__':
+    app.run()
+
